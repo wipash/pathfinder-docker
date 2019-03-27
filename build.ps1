49087473dcd48485fd8414756762c2b9bbb82663
Sean McGrath <sean@mcgrath.net.nz>
2019-03-26 20:08:13 -0400

    Clean up file tree

--- build.ps1
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/exodus4d/pathfinder.git
-docker run --rm --interactive ($PWD)
-
-docker build --rm -t pathfinder_test .
-docker images -q --filter label=stage=intermediate | ForEach-Object {docker rmi -f $_}
--- build.sh
+++
@@ -1,7 +0,0 @@
-#pathfinder_dir="$(pwd)/pathfinder"
-#mkdir -p $pathfinder_dir
-#docker run -ti --rm -e USERID=$UID -v ${HOME}:/root -v $pathfinder_dir:/git alpine/git clone https://github.com/exodus4d/pathfinder.git .
-#docker run --rm -v $pathfinder_dir:/app composer install --ignore-platform-reqs
-
-docker build --rm -t pathfinder_test .
-docker rmi -f $(docker images -q --filter label=stage=intermediate)
